abd137b5af2b372bcdbce6a1fc6349c3fa3f400e
Quang <tomminhquan1411@gmail.com>
2023-03-20 17:01:48 +0700

    Redux toolkit

---
+++ Redux-Took Kit/lesson1/src/App.js
@@ -0,0 +1,10 @@
+import Counter from "./feature/counter/Counter";
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ Redux-Took Kit/lesson2/src/features/users/usersSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = [
+  { id: "0", name: "John" },
+  { id: "1", name: "Mary" },
+  { id: "2", name: "Dave" },
+];
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+});
+
+export const selectAllUsers = (state) => state.users;
+export default usersSlice.reducer;
